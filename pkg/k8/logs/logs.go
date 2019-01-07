8221ede82c6fa08b9dd644c9d731299fa36f8401
Pablo Chico de Guzman <pablo@okteto.com>
2019-01-07 09:27:50 -0800

    Retry log streaming connection if it breaks

--- pkg/k8/logs/logs.go
+++ pkg/k8/logs/logs.go
@@ -3,30 +3,61 @@ package logs
 import (
 	"io"
 	"os"
+	"os/signal"
+	"sync"
+	"time"
 
+	"github.com/okteto/cnd/pkg/k8/deployments"
+	appsv1 "k8s.io/api/apps/v1"
 	apiv1 "k8s.io/api/core/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 )
 
-//Logs stremas logs from a container
-func Logs(c *kubernetes.Clientset, config *rest.Config, pod *apiv1.Pod, container string) error {
-	var tailLines int64
-	tailLines = 100
-	req := c.CoreV1().Pods(pod.Namespace).GetLogs(
-		pod.Name,
-		&apiv1.PodLogOptions{
-			Container:  container,
-			Timestamps: true,
-			Follow:     true,
-			TailLines:  &tailLines,
-		},
-	)
-	readCloser, err := req.Stream()
-	if err != nil {
-		return err
+//StreamLogs stremas logs from a container
+func StreamLogs(d *appsv1.Deployment, container string, c *kubernetes.Clientset, config *rest.Config, wg *sync.WaitGroup) {
+	defer wg.Done()
+	var readCloser io.ReadCloser
+	end := false
+
+	channel := make(chan os.Signal, 1)
+	signal.Notify(channel, os.Interrupt)
+	go func() {
+		<-channel
+		readCloser.Close()
+		end = true
+	}()
+
+	var wait time.Duration
+	for !end {
+		time.Sleep(wait * time.Second)
+		pod, err := deployments.GetCNDPod(d, c)
+		if err != nil {
+			if wait < 10 {
+				wait++
+			}
+			continue
+		}
+		var tailLines int64
+		tailLines = 100
+		req := c.CoreV1().Pods(pod.Namespace).GetLogs(
+			pod.Name,
+			&apiv1.PodLogOptions{
+				Container:  container,
+				Timestamps: true,
+				Follow:     true,
+				TailLines:  &tailLines,
+			},
+		)
+		readCloser, err = req.Stream()
+		if err != nil {
+			if wait < 10 {
+				wait++
+			}
+			continue
+		}
+		wait = 0
+		io.Copy(os.Stdout, readCloser)
 	}
-	defer readCloser.Close()
-	_, err = io.Copy(os.Stdout, readCloser)
-	return err
+	return
 }
